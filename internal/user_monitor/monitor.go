5598a9306dda575c79a88989d9ac508be0bb72c5
pelusa-v <razor0164@gmail.com>
2024-01-03 00:49:44 -0500

    User observer done

--- internal/user_monitor/monitor.go
+++ internal/user_monitor/monitor.go
@@ -1,4 +1,11 @@
 package user_monitor
 
 type Monitor interface {
+	RegisterObserver(obs UserObserver)
+	RemoveObserver(obs UserObserver)
+	NotifyUserCreated(obs UserObserver)
+}
+
+type UserMonitor struct {
+	Users []UserObserver
 }
--- internal/user_monitor/user.go
+++ internal/user_monitor/user.go
@@ -1,4 +1,26 @@
 package user_monitor
 
 type UserObserver interface {
+	AddNeighbor(neighbor User)
+	RemoveNeighbor(neighbor User)
+}
+
+type User struct {
+	Neighbors   []User
+	Name        string
+	UserMonitor Monitor
+	Id          int
+}
+
+func (u *User) AddNeighbor(neighbor User) {
+	u.Neighbors = append(u.Neighbors, neighbor)
+}
+
+func (u *User) RemoveNeighbor(neighbor User) {
+	for i, n := range u.Neighbors {
+		if n.Id == neighbor.Id {
+			n.Neighbors = append(n.Neighbors[:i], n.Neighbors[i+1:]...)
+			break
+		}
+	}
 }
